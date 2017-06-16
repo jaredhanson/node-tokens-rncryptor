de1ea13fa5bdf27c8b51cab4b4c3317905b69468
Jared Hanson <jaredhanson@gmail.com>
2017-06-15 17:04:59 -0700

    Add key-based encryption support.

--- test/seal.test.js
+++ test/seal.test.js
@@ -17,7 +17,11 @@ describe('seal', function() {
         
         switch (q.recipient.id) {
         case 'https://api.example.com/':
-          return cb(null, [ { secret: 'API-12abcdef7890abcdef7890abcdef' } ]);
+          if (q.usage == 'sign') {
+            return cb(null, [ { secret: 'API-90abcdef7890abcdef7890abcdef' } ]);
+          } else {
+            return cb(null, [ { secret: 'API-12abcdef7890abcdef7890abcdef' } ]);
+          }
         }
       });
       
@@ -114,6 +118,65 @@ describe('seal', function() {
         });
       });
     }); // encrypting to audience
+    
+    describe('encrypting to audience in key-based mode', function() {
+      var token;
+      before(function(done) {
+        var audience = [ {
+          id: 'https://api.example.com/'
+        } ];
+        
+        seal({ foo: 'bar' }, { audience: audience, password: false }, function(err, t) {
+          token = t;
+          done(err);
+        });
+      });
+      
+      after(function() {
+        keying.reset();
+      });
+      
+      it('should query for key', function() {
+        expect(keying.callCount).to.equal(2);
+        var call = keying.getCall(0);
+        expect(call.args[0]).to.deep.equal({
+          recipient: {
+            id: 'https://api.example.com/'
+          },
+          usage: 'encrypt',
+          algorithms: [ 'aes256-cbc' ]
+        });
+        
+        call = keying.getCall(1);
+        expect(call.args[0]).to.deep.equal({
+          recipient: {
+            id: 'https://api.example.com/'
+          },
+          usage: 'sign',
+          algorithms: [ 'hmac-sha256' ]
+        });
+      });
+      
+      it('should generate a token', function() {
+        expect(token.length).to.equal(88);
+        expect(token.substr(0, 1)).to.equal('A');
+      });
+      
+      /*
+      describe('verifying token', function() {
+        var claims;
+        before(function() {
+          var decrypted = RNCryptor.Decrypt(token, 'API-12abcdef7890abcdef7890abcdef');
+          claims = JSON.parse(decrypted.toString());
+        });
+        
+        it('should be valid', function() {
+          expect(claims).to.be.an('object');
+          expect(claims.foo).to.equal('bar');
+        });
+      });
+      */
+    }); // encrypting to audience in key-based mode
     
   }); // using defaults
   
